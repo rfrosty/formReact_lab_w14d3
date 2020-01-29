f7dae1ef49a0276c719ab3bb05dc63889346a8a0
Ryan <ryanfrost2015@gmail.com>
2020-01-29 15:31:03 +0000

    last commit from lab

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Story = (props) => {
   if (!props.story) return null;
   return (
-  <a href={props.story.url} target="blank">Link to Story</a>
+  <a href={props.story.url} target="blank">Link to title</a>
   )
 }
 export default Story;
